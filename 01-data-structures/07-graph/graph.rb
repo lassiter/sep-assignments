dc8a02c3a52d2fa5eb075ce47937d4b7855f951d
Lassiter Gregg <lassitergregg@gmail.com>
2018-09-26 17:26:22 -0500

    basic depth search setup troubleshooting algo returns

--- 01-data-structures/07-graph/graph.rb
+++ 01-data-structures/07-graph/graph.rb
@@ -6,21 +6,98 @@ class Graph
   attr_accessor :data
   attr_accessor :graph
   attr_accessor :stack
+  attr_reader :max_depth
 
-  def initialize(graph)
+  def initialize(data)
     @graph = graph
     @stack = stack
-    @data = data # Warehouse.new("data.csv")
+    @data = data
+    @max_depth = 6
   end
 
   # Write a function that begins at a provided Node object and returns an 
   # Array of at most six film titles that connect it to the Kevin_Bacon Node.
-  def find_kevin_bacon(starting_node)
+  def find_kevin_bacon(actor_name_string)
+    return nil if actor_name_string.nil? || actor_name_string === "nil"
+    actor_node = self.data.bsearch {|x| x.name === actor_name_string }
+
     # Find the Baconator
+    kevin_bacon = iterative_depth_search(actor_node)
+
   end
 
+  # Iterative Depth Search
+  def iterative_depth_search(root)
+    for i in 0..@max_depth
+      found, remaining = depth_limited_search(root, i)
+      binding.pry if found
+      if !found.nil? && found.kind_of?(Node)
+        binding.pry
+        return found
+      elsif !remaining
+        return -1
+      end
+      puts i
+    end
+    binding.pry
+    # node.film_actor_hash.keys.each do |film_title|
+    #   node.film_actor_hash[film_title].each do |child_node|
+    #     bacon_search(child_node)
+    #   end
+    # end
+  end
+
+  # Depth Limited Search
+  def depth_limited_search(node, depth)
+    if depth <= @max_depth
+      [nil, false]
+    end
+    if depth === 0
+      if node.name === "Kevin Bacon"
+        result = [node, true]
+        return result
+      elsif node.film_actor_hash.any?
+        result = [nil, true]
+        return result
+      else
+        result = [nil, false]
+        return result
+      end
+    elsif depth > 0 && depth <= @max_depth
+      node.film_actor_hash.keys.each do |film_title|
+        node.film_actor_hash[film_title].each do |child_node|
+          # If node child is Kevin Bacon, return the bacon;
+          # Else go one level deeper.
+          if child_node.name === "Kevin Bacon"
+            result = [child_node, true]
+            return result
+          elsif depth <= @max_depth
+            found, remaining = depth_limited_search(child_node, depth + 1)
+            result = [found, remaining]
+            return result
+          else
+            result = [nil, false]
+            return result
+          end
+        end # child_node loop
+      end # film_title loop
+    else
+      result = [nil, true]
+      return result
+    end
+  end
 
 end
+
+      #  any_remaining ← false
+      #  foreach child of node
+      #      found, remaining ← DLS(child, depth−1)
+      #      if found ≠ null
+      #          return (found, true)   
+      #      if remaining
+      #          any_remaining ← true    (At least one node found at depth, let IDDFS deepen)
+      #  return (null, any_remaining)
+
 
 # Helper Class to handle converting csv files to nodes
 # and their respective film_actor_hashes
@@ -44,7 +121,6 @@ class Warehouse
         node = Node.new(row[0], Hash.new)
         node.film_actor_hash[row[2]] = []
         @data << node
-        puts "Added #{node.inspect} and @data has #{data.length}"
       else
         if existing_node.film_actor_hash.include?(row[2])
           puts "duplicate movie addition for node: #{node.inspect}"
@@ -84,7 +160,6 @@ class Warehouse
       if potential_cast_node.film_actor_hash.keys.include?(movie_key) && !potential_cast_node.film_actor_hash[movie_key].include?(node)
         potential_cast_node.film_actor_hash[movie_key] << node
         node.film_actor_hash[movie_key] << potential_cast_node
-        puts "Linked #{potential_cast_node.name} and #{node.name} as castmates in #{movie_key}"
       end
     end
   end
--- 01-data-structures/07-graph/graph_spec.rb
+++ 01-data-structures/07-graph/graph_spec.rb
@@ -4,42 +4,64 @@ require_relative 'node'
 require_relative 'graph'
 
 RSpec.describe Graph, type: Class do
+  let(:warehouse) { Warehouse.new("data.csv") }
+  let(:graph) { Graph.new(warehouse.data) }
 
   describe 'should be able to find Kevin Bacon' do
-    
+    it 'should handle nil' do
+      subject = graph.find_kevin_bacon(nil)
+      expect(subject).to be nil
+    end
+
+    it 'should handle unknown strings' do
+      subject = graph.find_kevin_bacon("foo bar")
+      expect(subject).to be nil
+    end
+
     it 'in 1 degree' do
-      
+      # via Castmate Tom Hanks in Apollo 13
+      subject = graph.find_kevin_bacon("Tom Hanks")
+      binding.pry
+      expect(subject.name).to eq("Kevin Bacon")
     end
 
     it 'in 2 degrees' do
-      
+      # via Laurence Fishburne
+      subject = graph.find_kevin_bacon("Keanu Reeves")
+      expect(subject.name).to eq("Kevin Bacon")
     end
 
     it 'in 3 degrees' do
-      
+      # via Tom Bower -> Marcia Gay Harden
+      subject = graph.find_kevin_bacon("Anna Felix")
+      expect(subject.name).to eq("Kevin Bacon")
     end
 
     it 'in 4 degrees' do
-      
+      # via Jeff Bridges -> Tom Bower -> Marcia Gay Harden
+      subject = graph.find_kevin_bacon("Leslie Bibb")
+      expect(subject.name).to eq("Kevin Bacon")
     end
 
     it 'in 5 degrees' do
-      
+      # via Amy Adams -> Robin Williams -> Matt Damon -> Tom Hanks
+      subject = graph.find_kevin_bacon("Christian Bale")
+      expect(subject.name).to eq("Kevin Bacon")
     end
 
     it 'in 6 degrees' do
-      
+      # via Christian Bale -> Amy Adams -> Robin Williams -> Matt Damon -> Tom Hanks
+      subject = graph.find_kevin_bacon("Gary Oldman")
+      expect(subject.name).to eq("Kevin Bacon")
     end
 
   end
   describe 'should not be able to find Kevin Bacon' do
     
     it 'in 7 degrees' do
-      
-    end
-
-    it 'in 8 degrees or more' do
-      
+      # via Gary Oldman -> Christian Bale -> Amy Adams -> Robin Williams -> Matt Damon -> Tom Hanks
+      subject = graph.find_kevin_bacon("Giancarlo Giannini")
+      expect(subject.name).to eq("Kevin Bacon")
     end
 
   end

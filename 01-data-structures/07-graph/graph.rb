f23898a1c40ca56de5642245d75668689322aa00
Lassiter Gregg <lassitergregg@gmail.com>
2018-09-25 21:53:23 -0500

    Created basic structure for assignment.
    Found data from a IBM Bluemix project, credit: https://github.com/ukmadlz/6degrees-of-kevin-bacon/blob/master/data/movies-demo.csv

---
+++ 01-data-structures/07-graph/graph.rb
@@ -0,0 +1,18 @@
+require_relative 'node'
+
+class Graph
+  attr_accessor :graph
+  
+  def initialize(graph)
+    @graph = graph
+    @stack = stack
+  end
+
+  # Write a function that begins at a provided Node object and returns an 
+  # Array of at most six film titles that connect it to the Kevin_Bacon Node.
+  def find_kevin_bacon(starting_node)
+    # Find the Baconator
+  end
+
+
+end
---
+++ 01-data-structures/07-graph/graph_spec.rb
@@ -0,0 +1,9 @@
+include RSpec
+
+require_relative 'node'
+require_relative 'graph'
+
+RSpec.describe Graph, type: Class do
+
+
+end

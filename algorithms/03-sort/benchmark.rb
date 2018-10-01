42816d9817ee4db8649f1033b6c5912c0c8445b8
Lassiter Gregg <lassitergregg@gmail.com>
2018-10-01 03:11:54 -0500

    added bubble, insert, merge, selection sorts

--- algorithms/03-sort/benchmark.rb
+++ algorithms/03-sort/benchmark.rb
@@ -1,9 +1,13 @@
 require_relative "bucket_sort"
 require_relative "heap_sort"
 require_relative "quick_sort"
-unsorted_array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'].shuffle
-unsorted_num_array = Array.new(11) {|index| index = rand(1..100)}
+require_relative "merge_sort"
+require_relative "bubble_sort"
+require_relative "selection_sort"
+require_relative "insertion_sort"
 
-array = qs(unsorted_array)
-puts "#{array.kind_of?(Array)}"
-puts array
+
+unsorted_array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'].shuffle
+unsorted_num_array = Array.new(40) {|index| index = rand(1..100)}
+
+qs(unsorted_array)
